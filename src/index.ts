d22c8f61e187cf57789caafd3619ea92840acef9
MartinSchuhmacher <mschuhmacher@gmx.de>
2021-10-16 17:00:18 +0200

    feat: Adding simple server start

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, { Response } from 'express';
+import fs from 'fs';
+import path from 'path';
+
+const app = express();
+const port = 8081;
+
+app.listen(port, (): void => {
+   // check for thumbnail folder
+   const thumbnailPath: string = path.resolve(__dirname, '../assets/thumbnail');
+
+   // create synchronous if not existent
+   if (!fs.existsSync(thumbnailPath)) {
+      fs.mkdirSync(thumbnailPath);
+   }
+
+   console.log(`Server running smoothly on port ${port}!`);
+});
+
+app.get('/', (_, res: Response): void => {
+   res.sendStatus(200);
+});
+
+export default app;
