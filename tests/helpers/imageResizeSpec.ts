ac78607b0c8b81383a3914222a6700a291e33878
MartinSchuhmacher <mschuhmacher@gmx.de>
2021-10-20 21:21:06 +0200

    test: Adding tests with Jasmine

---
+++ tests/helpers/imageResizeSpec.ts
@@ -0,0 +1,28 @@
+import path from 'path';
+import imageHelper from '../../src/helpers/imageResize';
+
+const fullImagePath = path.resolve(__dirname, '../../assets/full/fox.jpg');
+const thumbnailImagePath = path.resolve(__dirname, '../../assets/thumbnail/fox.jpg');
+
+describe('imageResize function', (): void => {
+   it('returns a buffer after successfully resizing the image', async () => {
+      const imageBuffer: Buffer = await imageHelper.resizeImage({
+         fullImagePath,
+         thumbnailImagePath,
+         width: 200,
+         height: 100,
+      });
+      expect(imageBuffer).toBeInstanceOf(Buffer);
+   });
+
+   it('rejects promise if something went wrong during processing', async (): Promise<void> => {
+      await expectAsync(
+         imageHelper.resizeImage({
+            fullImagePath: '',
+            thumbnailImagePath,
+            width: 200,
+            height: 100,
+         }),
+      ).toBeRejected();
+   });
+});
